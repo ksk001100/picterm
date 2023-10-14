3e9d778dfd88c175a45857bbd9756d472200e262
omer <omerabdi@protonmail.com>
2023-10-14 10:17:41 +0300

    style search field

--- src/app/ui/search.rs
+++ src/app/ui/search.rs
@@ -1,10 +1,17 @@
-use tui::widgets::{Block, Borders, Paragraph};
+use tui::{
+    style::{Color, Style},
+    widgets::{Block, BorderType, Borders, Paragraph},
+};
 
 pub fn draw<'a, 'b>(search_term: &'a str) -> Paragraph<'b>
 where
     'a: 'b,
 {
-    let block = Block::default().title("Search").borders(Borders::ALL);
-    let paragraph = Paragraph::new(format!("{}█", search_term)).block(block);
-    paragraph
+    Paragraph::new(format!("{}█", search_term)).block(
+        Block::default()
+            .title("Search")
+            .borders(Borders::ALL)
+            .style(Style::default().fg(Color::White))
+            .border_type(BorderType::Plain),
+    )
 }
